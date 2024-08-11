7d9e5d5cc08f6e3f5c9e9e6dc8dd0e51b0dea137
Kasun Gayan <155733907+Kasun09@users.noreply.github.com>
2024-08-11 20:21:03 +0530

    Add files via upload

--- Library database.sql
+++ Library database.sql
@@ -23,3 +23,15 @@ contact bigint not null,
 email varchar(250) not null
 );
 
+create table IRBook(
+id int not null auto_increment primary key,
+std_enroll varchar (250) not null,
+std_name varchar (250) not null,
+ std_dep varchar (250) not null,
+ std_sem varchar (250) not null,
+ std_contact bigint not null,
+ std_email varchar (250) not null,
+ book_name varchar (250) not null,
+ book_issue_date varchar (250) not null,
+ Book_return_date varchar (250) 
+);
